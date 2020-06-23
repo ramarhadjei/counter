858958b875051a963e37ac8e18a51a6b0184fb7d
Rebecca Amarh-Adjei <37591664+ramarhadjei@users.noreply.github.com>
2020-06-23 15:29:31 -0400

    Added buttons to show increment that does not go beyond 20

--- src/App.js
+++ src/App.js
@@ -1,53 +1,61 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-import React, {Component} froom 'react';
-class HelloWorld extends Component {
-  render (){
-    return(
-      <div>Hello HelloWorld</div>
-    );
-  };
-};
-export default HelloWorld;
-
-import React, {Componet} from 'react';
+import React, {Component} from 'react';
 class Counter extends Component {
-  render(){
+  constructor(){
+    super();
+    this.state = {
+      count: 0,
+    }
+    this.toggle = {
+      on: true,
+      off: false
+    }
+  }
+  increment =()=> {
+    // this is where your code goes
+    if (this.state.count >= 20){
+    this.setState({ 
+      count: this.state.count = 20
+    })
+      
+    }else {
+      this.setState ({
+      count: this.state.count +1 
+      
+      }) 
+  } 
+  }
+  decrement = () => {
+    if (this.state.count <= 0){
+      this.setState({ 
+        count: this.state.count = 0
+      })
+    } else {
+      this.setState({
+        count: this.state.count -1
+      })
+    }
+  }
+  clear = ()=> {
+    
+    this.setState({
+      count: this.state.count = 0
+    })
+
+  }
+  render () {
     return(
-      <div></div>
-    )
+      <div className = "container"> 
+        <div className = "navBar"> Counter.js</div> 
+        <div className ="counter">
+          <h1>{this.state.count}</h1>
+          <button type = "button" onClick={this.increment}>Increment</button>
+          <button type = "button" onClick={this.decrement}>Decrement</button>
+          <button type = "button" onClick={this.clear}>clear</button>
+          
+        </div>
+      
+      </div>
+    );
   }
 }
 export default Counter;
-
-class Counter extends Component {
-  constructor()
-  super()
-  this.state = {
-    count: 0,
-  }
-}
