beac9417a8c2335e5a64957738b48da0b1c70ffe
Rebecca Amarh-Adjei <37591664+ramarhadjei@users.noreply.github.com>
2020-06-28 19:42:54 -0400

    Added toggle component to project

--- src/App.js
+++ src/App.js
@@ -4,53 +4,75 @@ class Counter extends Component {
     super();
     this.state = {
       count: 0,
-    }
-    this.toggle = {
-      on: true,
-      off: false
-    }
+      doubleCount: false,
+    };
   }
-  increment =()=> {
-    // this is where your code goes
-    if (this.state.count >= 20){
-    this.setState({ 
-      count: this.state.count = 20
-    })
-      
-    }else {
-      this.setState ({
-      count: this.state.count +1 
-      
-      }) 
-  } 
-  }
-  decrement = () => {
-    if (this.state.count <= 0){
-      this.setState({ 
-        count: this.state.count = 0
-      })
-    } else {
-      this.setState({
-        count: this.state.count -1
-      })
-    }
-  }
-  clear = ()=> {
-    
+
+  //this is where our methods will be defined
+
+  increment = () => {
+    if (this.state.doubleCount ===  false && this.state.count < 20) {
     this.setState({
-      count: this.state.count = 0
-    })
+      count: this.state.count + 1,
+        }); 
+    }else if (this.state.doubleCount === true && this.state.count <20){
+      this.setState({
+        count: this.state.count +2
+      })
+    } 
+  };
+    // this is where your code goes
+   
+     
+ 
+  
+  decrement = () => {
+
+    if(this.state.doubleCount === false && this.state.count > 0){
+    
+      this.setState({
+      count: this.state.count -1,
+      })
+    }else if (this.state.doubleCount === true && this.state.count >0){
+      this.setState({
+        count: this.state.count -2
+      })
+    }
+  }
+    
+  
+
+  
+  clear = () => {
+    this.setState({
+      count: this.state.count = 0.
+    });
+  };
+
+  double = () => {
+    if(this.state.doubleCount) {
+      this.setState({
+        doubleCount: false,
+      })
+      //console.log(this.state.doubleCount)
+    }else {
+      this.setState({
+        doubleCount: true,
+      })
+    }
 
   }
   render () {
     return(
       <div className = "container"> 
-        <div className = "navBar"> Counter.js</div> 
+        <div className = "navbar"> Counter.js</div> 
         <div className ="counter">
           <h1>{this.state.count}</h1>
           <button type = "button" onClick={this.increment}>Increment</button>
           <button type = "button" onClick={this.decrement}>Decrement</button>
           <button type = "button" onClick={this.clear}>clear</button>
+          <button type = "button" onClick={this.double}>
+            {this.state.doubleCount ? 'Double Count' : 'Single Count'}</button>
           
         </div>
       
